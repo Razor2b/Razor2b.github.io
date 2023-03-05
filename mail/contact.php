9cfeac786d8d3c91d83c3d345f20a605d0329224
Razor2b <rachelcbrinkley@outlook.com>
2023-03-05 16:02:43 -0500

    4th push updating contact code

--- mail/contact.php
+++ mail/contact.php
@@ -9,10 +9,10 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "info@example.com"; // Change this email to your //
+$to = "rachelcbrinkley@outlook.com";
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
+$header = "From: $name";
 $header .= "Reply-To: $email";	
 
 if(!mail($to, $subject, $body, $header))
